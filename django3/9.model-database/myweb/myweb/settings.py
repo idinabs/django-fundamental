863596faee697e6fec7b93f726c896c369192508
idin <idin@.com>
2020-09-23 21:34:43 +0800

    myweb1

--- django3/9.model-database/myweb/myweb/settings.py
+++ django3/9.model-database/myweb/myweb/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'myweb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
